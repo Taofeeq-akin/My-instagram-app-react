d57ea34637fc0af496af13cf95502b7f039a1a38
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-18 14:44:44 +0100

    Add edit form to bio component

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,6 +1,18 @@
 import profileIcon from "../assests/profileIcon.svg";
 
 const Bio = () => {
+  const editForm = (
+    <form className="edit-bio-form">
+      <input type="text" id="" placeholder="Your name" />
+      <input type="text" id="" placeholder="About you" />
+      <br />
+      <button className="cancel-button" type="button">
+        Cancel
+      </button>
+      <button type="button">Save</button>
+    </form>
+  );
+
   return (
     <section className="bio">
       <div
@@ -13,6 +25,10 @@ const Bio = () => {
       <div className="profile-info">
         <p className="name">Taofeeq Akin</p>
         <p className="about">Building web using react</p>
+        <button className="edit" type="button">
+          Edit
+        </button>
+        {editForm}
       </div>
     </section>
   );
